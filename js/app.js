f06ad5116b40056fd444721685684b1b41991a4d
Ron Clark <ronaldtclark@gmail.com>
2018-06-25 15:34:25 -0500

    created class and instantiated

---
+++ js/app.js
@@ -0,0 +1,66 @@
+
+class Tomagotchi {
+  constructor(name){
+    this.name = name;
+    this.age = 0;
+    this.hunger = 1;
+    this.sleepiness = 1;
+    this.boredom = 1;
+  };
+};
+
+const pop = new Tomagotchi ('Poppinfresh');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
